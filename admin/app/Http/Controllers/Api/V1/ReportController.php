b308a609e6366981ef71cf091093afafe52da1b3
Jasper Fernandez <jasperfernandez148@gmail.com>
2025-02-24 18:16:36 +0800

    add report controller and request

---
+++ admin/app/Http/Controllers/Api/V1/ReportController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+
+class ReportController extends Controller
+{
+    public function __invoke(ReportRequest $request)
+    {
+
+    }
+}
