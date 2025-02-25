4c0785019622eb03afa685b47a53a385716f641d
Jasper Fernandez <jasperfernandez148@gmail.com>
2025-02-25 21:18:44 +0800

    chore: add app ui

--- mobile/lib/config/router/app_router.dart
+++ mobile/lib/config/router/app_router.dart
@@ -3,12 +3,15 @@ import 'package:calamitech/core/auth/login/screens/login_screen.dart';
 import 'package:calamitech/core/auth/signup/screens/signup_screen.dart';
 import 'package:calamitech/core/splash/splash_screen.dart';
 import 'package:calamitech/features/home/screens/home_screen.dart';
+import 'package:calamitech/features/profile/screens/profile_screen.dart';
+import 'package:calamitech/features/report/screens/report_screen.dart';
+import 'package:calamitech/features/sos/screens/sos_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import '../../core/app/app_scaffold.dart';
 
 class AppRouter {
   GoRouter router = GoRouter(
-    initialLocation: RouteConstants.login,
     routes: [
       GoRoute(
           path: RouteConstants.splash,
@@ -25,11 +28,33 @@ class AppRouter {
           pageBuilder: (context, state) {
             return const MaterialPage(child: SignupScreen());
           }),
-      GoRoute(
-          path: RouteConstants.home,
-          pageBuilder: (context, state) {
-            return const MaterialPage(child: HomeScreen());
-          }),
+      ShellRoute(
+        builder: (context, state, child) => AppScaffold(child: child),
+        routes: [
+          GoRoute(
+              path: RouteConstants.home,
+              pageBuilder: (context, state) {
+                return const MaterialPage(child: HomeScreen());
+              }),
+          GoRoute(
+              path: RouteConstants.profile,
+              pageBuilder: (context, state) {
+                return const MaterialPage(child: ProfileScreen());
+              }),
+          GoRoute(
+              path: RouteConstants.report,
+              pageBuilder: (context, state) {
+                return const MaterialPage(child: ReportScreen());
+              }),
+          GoRoute(
+              path: RouteConstants.sos,
+              pageBuilder: (context, state) {
+                return const MaterialPage(child: SOSScreen());
+              }),
+        ],
+      ),
     ],
   );
 }
+
+
