61802b1366bf1fc9451ce0e4fd666602804a2543
Jasper Fernandez <jasperfernandez148@gmail.com>
2025-03-09 17:34:07 +0800

    test

--- mobile/lib/constants/api_paths.dart
+++ mobile/lib/constants/api_paths.dart
@@ -1,7 +1,11 @@
 class ApiPaths {
-  static const String localApiUrl = 'http://10.0.2.2:8000/api/v1';
-  static const String wifiApiUrl = 'http://192.168.245.59:8000/api/v1';
+  static const String emulatorIP = '10.0.2.2';
+  static const String wifiIP = '192.168.1.2';
+  static const String port = '8000';
+  static const String localApiUrl = 'http://$emulatorIP:$port/api/v1';
+  static const String wifiApiUrl = 'http://$wifiIP:$port/api/v1';
   static const String productionApiUrl = 'https://domain.tld/api/v1';
+
   static const String getUser = '/user';
   static const String login = '/login';
   static const String signup = '/signup';
