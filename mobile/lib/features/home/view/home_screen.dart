861653b50c980d130a21c7a63746f3ea1c868178
Jasper Fernandez <jasperfernandez148@gmail.com>
2025-04-11 16:54:57 +0800

    refactor

--- mobile/lib/features/home/view/home_screen.dart
+++ mobile/lib/features/home/view/home_screen.dart
@@ -1,11 +1,9 @@
-import 'package:calamitech/config/theme/app_theme.dart';
-import 'package:calamitech/constants/asset_paths.dart';
-import 'package:calamitech/constants/route_constants.dart';
+import 'package:calamitech/core/location/cubit/location_cubit.dart';
+import 'package:calamitech/features/home/view/calamity_tips.dart';
+import 'package:calamitech/features/home/view/featured_sos.dart';
+import 'package:calamitech/features/home/view/report_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-
-import '../home.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,117 +15,24 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
-    context.read<SosRecoReportsBloc>().add(SosRecoReportsFetched());
-    context.read<SosFeaturedReportsBloc>().add(SosFeaturedReportsFetched());
+    context.read<LocationCubit>().startLocationUpdates();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Flexible(
-            child: BlocBuilder<SosFeaturedReportsBloc, SosFeaturedReportsState>(
-              builder: (context, state) {
-                if (state is SosFeaturedReportsLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-
-                if (state is SosFeaturedReportsLoaded) {
-                  return SizedBox(
-                    height: 300,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: state.sosFeaturedReports.length,
-                      itemBuilder: (context, index) {
-                        return SosCard(
-                          sosReport: state.sosFeaturedReports[index],
-                          fullWidth: true,
-                        );
-                      },
-                    ),
-                  );
-                }
-
-                if (state is SosFeaturedReportsError) {
-                  return Center(
-                    child: Text(state.message),
-                  );
-                }
-
-                return const SizedBox();
-              },
-            ),
-          ),
-          GridView.count(
-            crossAxisCount: 2,
-            shrinkWrap: true,
-            childAspectRatio: 2.0,
-            children: [
-              Tip(color: AppTheme.primaryColor, image: AssetPaths.fire, title: 'Fire Tips', route: RouteConstants.fireTips),
-              Tip(color: Colors.green[400], image: AssetPaths.home, title: 'Flood Tips', route: RouteConstants.floodTips),
-              Tip(color: AppTheme.primaryColor, image: AssetPaths.safety, title: 'Safety Tips', route: RouteConstants.safetyTips),
-              const Tip(color: Colors.blueGrey, image: AssetPaths.more, title: 'More Tips', route: RouteConstants.tips),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'Recommendations for you',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  GoRouter.of(context).go(RouteConstants.sosReports);
-                },
-                child: const Text('see more'),
-              ),
-            ],
-          ),
-          Flexible(
-            child: BlocBuilder<SosRecoReportsBloc, SosRecoReportsState>(
-              builder: (context, state) {
-                if (state is SosRecoReportsLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-
-                if (state is SosRecoReportsLoaded) {
-                  return SizedBox(
-                    height: 250,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: state.sosRecoReports.length,
-                      itemBuilder: (context, index) {
-                        return SosCard(
-                          sosReport: state.sosRecoReports[index],
-                        );
-                      },
-                    ),
-                  );
-                }
-
-                if (state is SosRecoReportsError) {
-                  return Center(
-                    child: Text(state.message),
-                  );
-                }
-
-                return const SizedBox();
-              },
-            ),
-          ),
-        ],
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FeaturedSos(),
+            CalamityTips(),
+            ReportForm(),
+          ],
+        ),
       ),
     );
   }
