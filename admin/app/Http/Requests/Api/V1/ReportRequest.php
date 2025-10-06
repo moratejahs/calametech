b16bfe636b3425d92fa0129b652e7bfead6be4ef
jahsmorate <102463116+moratejahs@users.noreply.github.com>
2025-10-06 22:00:32 +0800

    dte

--- admin/app/Http/Requests/Api/V1/ReportRequest.php
+++ admin/app/Http/Requests/Api/V1/ReportRequest.php
@@ -38,7 +38,7 @@ class ReportRequest extends FormRequest
     {
         return [
             'description' => ['required'],
-            'ai_tips' => ['ai_tips'],
+            'ai_tips' => ['nullable'],
             'image' => ['nullable', 'image', 'max:12288'], // max 12mb
             'type' => ['required', 'in:fire,flood'],
             'lat' => ['required'],
